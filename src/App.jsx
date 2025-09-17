add4f7260a466fd434dd93222741ede65f2916c1
Komoliddin <obodullaevkk07@gmail.com>
2025-09-17 18:54:18 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Main from './components/Menu';
 import FlashSales from './components/Flash-Sales';
 import Categories from './components/categories';
